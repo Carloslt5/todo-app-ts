05ef5645bdebf54aff42bfa981cfdf0db30a26d7
Carloslt <carloslt.proyectos@gmail.com>
2024-05-15 15:18:37 +0200

    🔨ask: update home page, add text

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -4,10 +4,11 @@ const HomePage = () => {
   const { darkMode } = useThemeContext()
 
   return (
-    <div className='container px-2 mx-auto max-w-7xl'>
-      <h1 className='text-3xl font-bold dark:text-white'>
-        Holis {darkMode ?'🏄 ': '🛌'}
+    <div className='container px-2 mx-auto max-w-7xl dark:text-white'>
+      <h1 className='text-3xl font-bold'>
+        Welcome {darkMode ?'🏄 ': '🛌'}
       </h1>
+      <h2 className='mt-4'>Manage all your tasks simply, streamlining your workflow, and increasing productivity</h2>
     </div>
   )
 }
